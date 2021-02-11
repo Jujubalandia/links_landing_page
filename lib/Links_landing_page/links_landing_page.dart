d8b8d6ca7720a19faef9e3d0af03f534691a0379
brunormoura <brunormoura@gmail.com>
2021-02-11 00:00:50 -0300

    StreamBuilder Progress Indicator

--- lib/Links_landing_page/links_landing_page.dart
+++ lib/Links_landing_page/links_landing_page.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 import 'button_link.dart';
@@ -7,35 +8,43 @@ import 'footer.dart';
 class LinksLandingPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    var material = Material(
-      child: Column(
-        children: [
-          SizedBox(height: 8),
-          CircleAvatar(
-            backgroundImage: NetworkImage(imageUrl),
-            backgroundColor: Colors.white,
-            radius: 48,
-          ),
-          SizedBox(height: 12),
-          Text(
-            '@_Bera_',
-            style: TextStyle(
-              fontSize: 16,
-              color: Colors.grey,
+    return StreamBuilder<QuerySnapshot>(
+        stream: FirebaseFirestore.instance.collection('links').snapshots(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          return Material(
+            child: Column(
+              children: [
+                SizedBox(height: 35),
+                CircleAvatar(
+                  backgroundImage: NetworkImage(imageUrl),
+                  backgroundColor: Colors.white,
+                  radius: 48,
+                ),
+                SizedBox(height: 12),
+                Text(
+                  '@_Bera_',
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.grey,
+                  ),
+                ),
+                SizedBox(height: 12),
+                for (var document in documents)
+                  ButtonLink(
+                    title: document.title,
+                    url: document.url,
+                  ),
+                Spacer(),
+                Footer(),
+                SizedBox(height: 23),
+              ],
             ),
-          ),
-          SizedBox(height: 12),
-          for (var document in documents)
-            ButtonLink(
-              title: document.title,
-              url: document.url,
-            ),
-          Spacer(),
-          Footer(),
-          SizedBox(height: 23),
-        ],
-      ),
-    );
-    return material;
+          );
+        });
   }
 }
